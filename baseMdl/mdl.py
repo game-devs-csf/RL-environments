51ee23664c16c9d6af13c1cd30db7579f769314f
Andres Briseno <A01352283@tec.mx>
2023-11-23 20:34:28 -0600

    Runs pre-commit through all files in the project

--- baseMdl/mdl.py
+++ baseMdl/mdl.py
@@ -1,13 +1,11 @@
-import os, sys
-import time
 import random
 import numpy as np
 import pygame
 
-#import pygame
-#import sys
+# import pygame
+# import sys
 #
-#class Env:
+# class Env:
 #    """
 #        Description:
 #            Base class for environments
@@ -58,15 +56,16 @@ import pygame
 #        sys.exit()
 
 
-class Model():
+class Model:
     """
-        Description:
-            Base class for models
+    Description:
+        Base class for models
 
-        Parameters:
-            modelName (str): The name of the model
-            environment (Env): The environment of the model
+    Parameters:
+        modelName (str): The name of the model
+        environment (Env): The environment of the model
     """
+
     def __init__(self, mdl_name, environment):
         self.mdlName = mdlName
         self.environment = environment
@@ -77,7 +76,10 @@ class Model():
 
     def discretize(obs, lower_bounds, upper_bounds, buckets):
         """Discretize the observation space into buckets."""
-        ratios = [(ob + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i, ob in enumerate(obs)]
+        ratios = [
+            (ob + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i])
+            for i, ob in enumerate(obs)
+        ]
         new_obs = [int(round((buckets[i] - 1) * ratios[i])) for i in range(len(obs))]
         new_obs = [min(buckets[i] - 1, max(0, new_obs[i])) for i in range(len(obs))]
         return tuple(new_obs)
@@ -99,29 +101,35 @@ class Model():
 
         print(f"Training model from scratch for {episodes - 1} episodes...")
         for episode in range(episodes):
-            
-            current_state = discretize(env.reset(pygame.HIDDEN), lower_bounds, upper_bounds, buckets)
+            current_state = discretize(
+                env.reset(pygame.HIDDEN), lower_bounds, upper_bounds, buckets
+            )
 
             total_reward = 0
 
-            epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
+            epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(
+                -decay * episode
+            )
 
             while env.running:
-
                 exp_tradeoff = random.uniform(0, 1)
 
                 if exp_tradeoff > epsilon:
                     action = np.argmax(q_table[current_state])
                 else:
                     action = random.choice(env.action_space)
-                
+
                 observation, reward, done = env.step(action)
 
                 new_state = discretize(observation, lower_bounds, upper_bounds, buckets)
 
                 total_reward += reward
-                
-                q_table [current_state][action] += alpha * (reward + gamma * np.max(q_table[new_state]) - q_table[current_state][action])
+
+                q_table[current_state][action] += alpha * (
+                    reward
+                    + gamma * np.max(q_table[new_state])
+                    - q_table[current_state][action]
+                )
 
                 current_state = new_state
 
@@ -131,7 +139,9 @@ class Model():
                     env.running = False
 
             if not episode % 100:
-                print(f"Episode: {episode} | Reward: {total_reward} | Epsilon: {epsilon}")
+                print(
+                    f"Episode: {episode} | Reward: {total_reward} | Epsilon: {epsilon}"
+                )
 
         return q_table
 
@@ -141,14 +151,17 @@ class Model():
             try:
                 return np.load(mdl_file, allow_pickle=False)
             except OSError:
-                print(f"The input file {mdl_file} doesn't exist "
-                      f"or cannot be read.")
+                print(f"The input file {mdl_file} doesn't exist " f"or cannot be read.")
             except ValueError:
-                print(f"The file {mdl_file} contains an object array, but can't "
-                      f"be read due to allow_pickle=False")
+                print(
+                    f"The file {mdl_file} contains an object array, but can't "
+                    f"be read due to allow_pickle=False"
+                )
             except EOFError:
-                print(f"All data has already been read from file {mdl_file}.\n"
-                      f"Can't read an empty file.")
+                print(
+                    f"All data has already been read from file {mdl_file}.\n"
+                    f"Can't read an empty file."
+                )
 
     def watch_trained_model(mdl_q_table):
         current_state = discretize(env.reset(), lower_bounds, upper_bounds, buckets)
@@ -181,6 +194,5 @@ class Model():
                 mdl_file = input("Enter model file name: ")
                 np.save(mdl_file, mdl_q_table)
                 return
-            
-            print("Please enter 'y' or 'n'.\n"
-                  "Or press ENTER for default 'n'.")
+
+            print("Please enter 'y' or 'n'.\n" "Or press ENTER for default 'n'.")
--- examples/cartpole_tests.py
+++ examples/cartpole_tests.py
@@ -1,5 +1,4 @@
 import os, sys
-import time
 import random
 import numpy as np
 import pygame
@@ -8,19 +7,24 @@ import pygame
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the absolute path to the project's root directory
-project_root = os.path.abspath(os.path.join(current_dir, '..'))
+project_root = os.path.abspath(os.path.join(current_dir, ".."))
 
 # Add the project's root directory to sys.path
 sys.path.append(project_root)
 
 from cartPole.cartpole import CartpoleEnv
 
+
 # This function discretizes the observation space into buckets
 def discretize(obs, lower_bounds, upper_bounds, buckets):
-    ratios = [(ob + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i, ob in enumerate(obs)]
+    ratios = [
+        (ob + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i])
+        for i, ob in enumerate(obs)
+    ]
     new_obs = [int(round((buckets[i] - 1) * ratios[i])) for i in range(len(obs))]
     new_obs = [min(buckets[i] - 1, max(0, new_obs[i])) for i in range(len(obs))]
     return tuple(new_obs)
+
 
 # Create an instance of the Cartpole environment
 env = CartpoleEnv("Cartpole")
@@ -28,7 +32,12 @@ env = CartpoleEnv("Cartpole")
 upper_bounds = [4.8, 3.4, 0.42, 3.4]
 lower_bounds = [-4.8, -3.4, -0.42, -3.4]
 
-buckets = (1, 1, 6, 5,)
+buckets = (
+    1,
+    1,
+    6,
+    5,
+)
 
 q_table = np.zeros(buckets + (len(env.action_space),))
 
@@ -44,29 +53,31 @@ max_epsilon = 1.0
 decay = 0.01
 
 for episode in range(episodes):
-    
-    current_state = discretize(env.reset(pygame.HIDDEN), lower_bounds, upper_bounds, buckets)
+    current_state = discretize(
+        env.reset(pygame.HIDDEN), lower_bounds, upper_bounds, buckets
+    )
 
     total_reward = 0
 
     epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay * episode)
 
     while env.running:
-
         exp_tradeoff = random.uniform(0, 1)
 
         if exp_tradeoff > epsilon:
             action = np.argmax(q_table[current_state])
         else:
             action = random.choice(env.action_space)
-        
+
         observation, reward, done = env.step(action)
 
         new_state = discretize(observation, lower_bounds, upper_bounds, buckets)
 
         total_reward += reward
-        
-        q_table [current_state][action] += alpha * (reward + gamma * np.max(q_table[new_state]) - q_table[current_state][action])
+
+        q_table[current_state][action] += alpha * (
+            reward + gamma * np.max(q_table[new_state]) - q_table[current_state][action]
+        )
 
         current_state = new_state
 
