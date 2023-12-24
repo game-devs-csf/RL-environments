3717389e65e8eaff95a112453e86232f37a4ba2b
BassedWarrior <bassedwarrior724@outlook.com>
2023-12-23 23:27:14 -0600

    Initiol mdl.py file
    Initial version of a base model module.
    
    Missing testing and integration with rest of the code.
    Follow-up commits should include an integration with Cartpole game for
    testing along with an example which aims to replicate existing behaviour
    on curret examples.

--- baseMdl/mdl.py
+++ baseMdl/mdl.py
@@ -2,61 +2,8 @@ import random
 import numpy as np
 import pygame
 
-# import pygame
-# import sys
-#
-# class Env:
-#    """
-#        Description:
-#            Base class for environments
-#
-#        Parameters:
-#            envName (str): The name of the environment
-#            width (int): The width of the environment window
-#            height (int): The height of the environment window
-#    """
-#    def __init__(self, envName, width = 500, height = 500):
-#        self.envName = envName
-#        self.action_space = None
-#        self.observation_space = None
-#        self.env = None
-#        self.running = False
-#        self.window_size = (width, height)
-#        self.reward = None
-#
-#    def step(self, action):
-#        pass
-#
-#    def reset(self, show_display = pygame.SHOWN):
-#        """
-#            Description:
-#                Resets the environment to its initial state
-#        """
-#        pygame.init()
-#        pygame.display.set_caption(self.envName)
-#        self.env = pygame.display.set_mode(self.window_size, pygame.HWSURFACE | pygame.DOUBLEBUF | show_display)
-#        self.running = True
-#
-#    def render(self):
-#        """
-#            Description:
-#                Renders the environment
-#        """
-#        for event in pygame.event.get():
-#            if event.type == pygame.QUIT:
-#                self.running = False
-#            elif event.type == pygame.KEYDOWN:
-#                if event.key == pygame.K_ESCAPE:
-#                    self.running = False
-#
-#        pygame.display.flip()
-#
-#    def close(self):
-#        pygame.quit()
-#        sys.exit()
 
-
-class Model:
+class Mdl:
     """
     Description:
         Base class for models
@@ -67,12 +14,23 @@ class Model:
     """
 
     def __init__(self, mdl_name, environment):
+        # Name and Env
         self.mdlName = mdl_name
         self.env = environment
+        # The model itself
+        self.q_table = None
+        # For discretizing
         self.upper_bounds = None
         self.lower_bounds = None
         self.buckets = None
-        self.q_table = None
+        # For training
+        self.episodes = None
+        self.alpha = None
+        self.gamma = None
+        # For training exploration
+        self.min_epsilon = None
+        self.max_epsilon = None
+        self.decay = None
 
     def discretize(obs, lower_bounds, upper_bounds, buckets):
         """Discretize the observation space into buckets."""
@@ -86,29 +44,18 @@ class Model:
 
     def train_from_scratch(self):
         """Train new model with brand new q_table"""
-        q_table = np.zeros(self.buckets + (len(self.env.action_space),))
+        self.q_table = np.zeros(self.buckets + (len(self.env.action_space),))
 
-        episodes = 501
-
-        alpha = 0.1
-        gamma = 0.9
-
-        # exploration
-        epsilon = 1.0
-        """ min_epsilon = 0.1
-        max_epsilon = 1.0
-        decay = 0.01 """
-
-        print(f"Training model from scratch for {episodes - 1} episodes...")
-        for episode in range(episodes):
+        print(f"Training model from scratch for {self.episodes - 1}" f"episodes...")
+        for episode in range(self.episodes):
             current_state = self.discretize(
                 self.environment.reset(pygame.HIDDEN),
                 self.lower_bounds,
                 self.upper_bounds,
                 self.buckets,
             )
-            while self.environment.running:
-                total_reward = 0
+
+            total_reward = 0
 
             epsilon = self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(
                 -self.decay * episode
@@ -118,7 +65,7 @@ class Model:
                 exp_tradeoff = random.uniform(0, 1)
 
                 if exp_tradeoff > epsilon:
-                    action = np.argmax(q_table[current_state])
+                    action = np.argmax(self.q_table[current_state])
                 else:
                     action = random.choice(self.env.action_space)
 
@@ -130,33 +77,33 @@ class Model:
 
                 total_reward += reward
 
-                self.q_table[current_state][action] += alpha * (
+                self.q_table[current_state][action] += self.alpha * (
                     reward
-                    + gamma * np.max(self.q_table[new_state])
+                    + self.gamma * np.max(self.q_table[new_state])
                     - self.q_table[current_state][action]
                 )
 
                 current_state = new_state
-
-                # env.render()
 
                 if done:
                     self.env.running = False
 
             if not episode % 100:
                 print(
-                    f"Episode: {episode} | Reward: {total_reward} | Epsilon: {epsilon}"
+                    f"Episode: {episode} | Reward: {total_reward} "
+                    f"| Epsilon: {epsilon}"
                 )
 
-        return q_table
-
-    def import_model():
+    def import_model(self):
         while True:
             mdl_file = input("Enter model file name (.npy): ")
             try:
-                return np.load(mdl_file, allow_pickle=False)
+                self.q_table = np.load(mdl_file, allow_pickle=False)
             except OSError:
-                print(f"The input file {mdl_file} doesn't exist " f"or cannot be read.")
+                print(
+                    f"The input file {mdl_file}.npy doesn't exist "
+                    f"or cannot be read."
+                )
             except ValueError:
                 print(
                     f"The file {mdl_file} contains an object array, but can't "
@@ -168,17 +115,17 @@ class Model:
                     f"Can't read an empty file."
                 )
 
-    def watch_trained_model(self, mdl_q_table):
+    def watch_trained_model(self):
         current_state = self.discretize(
             self.env.reset(), self.lower_bounds, self.upper_bounds, self.buckets
         )
         total_reward = 0
 
         print("Watching trained model...")
-        while True:
+        while self.env.running:
             self.env.render()
 
-            action = np.argmax(mdl_q_table[current_state])
+            action = np.argmax(self.q_table[current_state])
 
             observation, reward, done = self.env.step(action)
 
@@ -191,17 +138,18 @@ class Model:
             current_state = new_state
 
             if done:
-                break
+                self.env.running = False
+                print(f"Total Reward: {total_reward}")
         print("Finished watching trained model...")
 
-    def ask_to_save_model(mdl_q_table):
+    def ask_to_save_model(self):
         while True:
             save = input("Save model? [y/N]: ")
             if save in ("", "N", "n"):
                 return
             elif save in ("Y", "y"):
                 mdl_file = input("Enter model file name: ")
-                np.save(mdl_file, mdl_q_table)
+                np.save(mdl_file, self.q_table)
                 return
 
             print("Please enter 'y' or 'n'.\n" "Or press ENTER for default 'n'.")
